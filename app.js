8d37e9d64e1ffffff079c01faeeb5893e8ac5756
SPANNACCINI Céline <celine.spannaccini@cote-azur.aeroport.fr>
2022-01-31 18:45:50 +0100

    added ejs and dynamic content

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.use(express.static('public'));
+
+const path = require('path');
+const fs = require('fs');
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.use(express.static('public')); // to serving static files
+app.use(express.urlencoded({extended:false}));
 
 app.get('/', function(req,res){
-    const htmlFilePath = path.join(__dirname,'views', 'index.html');
-    res.sendFile(htmlFilePath); 
+    res.render('index');
 });
 
 app.get('/restaurants', function(req,res){
-    const htmlFilePath = path.join(__dirname,'views', 'restaurants.html');
-    res.sendFile(htmlFilePath); 
+     const filePath = path.join(__dirname,'data','restaurants.json');
+    const readData = fs.readFileSync(filePath);
+    const sharedRestaurants = JSON.parse(readData);
+    res.render('restaurants', {numberOfRestaurants : sharedRestaurants.length, restaurants: sharedRestaurants});
+  
 });
 
 app.get('/recommend', function(req,res){
-    const htmlFilePath = path.join(__dirname,'views', 'recommend.html');
-    res.sendFile(htmlFilePath); 
+    res.render('recommend');
 });
+
+app.post('/recommend', function(req,res){
+    const newRestaurant = req.body;
+    const filePath = path.join(__dirname,'data','restaurants.json');
+
+    const readData = fs.readFileSync(filePath);
+    const sharedRestaurants = JSON.parse(readData);
+    console.log(sharedRestaurants);
+
+    sharedRestaurants.push(newRestaurant);
+    
+    const storedRestaurants = JSON.stringify(sharedRestaurants);
+    fs.writeFileSync(filePath, storedRestaurants);
+    
+    res.redirect('/confirm');
+})
 
 app.get('/confirm', function(req,res){
-    const htmlFilePath = path.join(__dirname,'views', 'confirm.html');
-    res.sendFile(htmlFilePath); 
+    res.render('confirm');
 });
 
-
 app.get('/about', function(req,res){
-    const htmlFilePath = path.join(__dirname,'views', 'about.html');
-    res.sendFile(htmlFilePath); 
+    res.render('about'); 
 });
 
 
